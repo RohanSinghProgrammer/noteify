4cddbb38c5fdddbf09e30fa195e5a17b21f08f4e
Rohan Singh <ff3870422@gmail.com>
2025-12-07 12:34:41 +0530

    minor changes

--- main.go
+++ main.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
+var (
+	cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("200"))
+	vault       string
+)
+
 type model struct {
 	newFileInput          textinput.Model
 	isNewFileInputVisible bool
+	currentFile           *os.File
+	contentTextarea       textarea.Model
 }
 
-var (
-	cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("200"))
-)
-
 func initialModel() model {
+
+	// create base directory to save notes
+	err := os.MkdirAll(vault, 0750)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// initialize new file input
 	ti := textinput.New()
 	ti.Placeholder = "What you like to call it?"
 	ti.Focus()
@@ -28,9 +41,16 @@ func initialModel() model {
 	ti.TextStyle = cursorStyle
 	ti.PromptStyle = cursorStyle
 
+	// initialize content textarea
+	ta := textarea.New()
+	ta.Placeholder = "Write something inside your file"
+	ta.ShowLineNumbers = false
+	ta.Focus()
+
 	return model{
 		newFileInput:          ti,
 		isNewFileInputVisible: false,
+		contentTextarea:       ta,
 	}
 }
 
@@ -48,13 +68,17 @@ func (m model) View() string {
 		Background(lipgloss.Color("51")).
 		Padding(0, 2)
 
-		// Texts
+	// Texts
 	header := headerStyle.Render("Welcome to Noteify!")
 	view := ""
 	help := helpStyle.Render("Ctrl+N: new file | Ctrl+L: list | Ctrl+S: save | Ctrl+Q: quit | Esc: back/save")
 
 	if m.isNewFileInputVisible {
 		view = m.newFileInput.View()
+	}
+
+	if m.currentFile != nil {
+		view = m.contentTextarea.View()
 	}
 
 	s := fmt.Sprintf("\n%s\n\n%s\n\n%s", header, view, help)
@@ -69,16 +93,44 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c", "esc":
 			return m, tea.Quit
+			
 		case "ctrl+n":
 			m.isNewFileInputVisible = true
+			return m, nil
+
+		case "ctrl+s":
+			if m.currentFile == nil {
+				break
+			}
+			return m, nil
+
+		case "enter":
+			filename := m.newFileInput.Value()
+			filepath := fmt.Sprintf("%s/%s.md", vault, filename)
+
+			if _, err := os.Stat(filepath); err == nil {
+				return m, nil
+			}
+
+			f, err := os.Create(filepath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			m.currentFile = f
+			m.newFileInput.SetValue("")
+			m.isNewFileInputVisible = false
 			return m, nil
 		}
 
 	}
 	if m.isNewFileInputVisible {
 		m.newFileInput, cmd = m.newFileInput.Update(msg)
+		return m, cmd
+	}
+	if m.currentFile != nil {
+		m.contentTextarea, cmd = m.contentTextarea.Update(msg)
 		return m, cmd
 	}
 
@@ -88,6 +140,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
 	return nil
+}
+
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Error getting home directory", err.Error())
+	}
+	vault = fmt.Sprintf("%s/.noteify", homeDir)
 }
 
 func main() {
